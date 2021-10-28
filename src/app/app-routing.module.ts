43b70e620fbd2a3a566fdda5680064a4b17c3042
Jmedinar4 <jmedinar4@miumg.edu.gt>
2021-10-27 18:47:29 -0600

    Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductoComponent } from './producto/producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'producto',component:ProductoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
